a5f2c3b9091fa479b6bf960b8dd8e874be3699ad
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-01-07 12:06:41 +0100

    Pantalla bienvenida y pantalla reglas. Traducción.

---
+++ app/src/main/java/com/example/mancala/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mancala
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val howToPlayBttn: Button = findViewById(R.id.howToPlayBttn)
+        val playBttn: Button = findViewById(R.id.playBttn)
+
+        howToPlayBttn.setOnClickListener {
+            startActivity(Intent(this, HowToPlayActivity::class.java))
+        }
+
+        playBttn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
